b299f7bae413cd89f0946e6d8503f133a2e6773e
Jiajie Li <jiajieli9991@gmail.com>
2021-01-30 04:58:45 +1100

    Simplify the code

--- ascii.py
+++ ascii.py
@@ -4,11 +4,10 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('file')     # path of the input file
-    parser.add_argument('-o', '--output')   # path of the output file
-    parser.add_argument('--width', type = int, default = 80) # the width of the output painting
-    parser.add_argument('--height', type = int, default = 80) # the height of the output painting
-
+    parser.add_argument('file', help="path of the input file")   
+    parser.add_argument('-o', '--output', help="path of the output file")  
+    parser.add_argument('--width', type = int, default = 80, help="the width of the output painting,by default 80")
+    parser.add_argument('--height', type = int, default = 80, help="the height of the output painting by default 80") 
     args = parser.parse_args()
 
     IMG = args.file
@@ -17,26 +16,19 @@ def main():
     OUTPUT = args.output
 
     ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
-
-    # map 256 grayscale values to 70 ASCII characters
-    def get_char(r,g,b,alpha = 256):
-        if alpha == 0:
-            return ' '
-        length = len(ascii_char)
-        gray = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
-
-        unit = (256.0 + 1)/length
-        return ascii_char[int(gray/unit)]
+    length = len(ascii_char)
 
     im = Image.open(IMG)
     im = im.resize((WIDTH,HEIGHT), Image.NEAREST)
-    im = im.convert('RGB') # ensure the value of each pixel is a tuple
+    im = im.convert('L') 
 
     txt = ""
 
+    # map 256 grayscale values to ASCII characters in the list
     for i in range(HEIGHT):
         for j in range(WIDTH):
-            txt += get_char(*im.getpixel((j,i)))
+            gray = im.getpixel((j,i))
+            txt += ascii_char[int(gray/255*(length-1))]
         txt += '\n'
 
     print(txt)
